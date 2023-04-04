3c5595d8e1b186d1ad17b4f843ebcfdeb4536e00
Diogo Belarmino Miranda Da Silva <dbelarmi@c3r9p3.42sp.org.br>
2023-04-04 14:54:16 -0300

    corrigido a libft

--- client.c
+++ client.c
@@ -3,80 +3,78 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbelarmi <dbelarmi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dbelarmi <dbelarmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 08:18:15 by dbelarmi          #+#    #+#             */
-/*   Updated: 2023/04/03 21:53:24 by dbelarmi         ###   ########.fr       */
+/*   Updated: 2023/04/04 13:29:14 by dbelarmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int  kill_status = 0;
-
-void print_error(char *message)
+void	print_error(char *message)
 {
-    ft_putstr(message);
-    exit(EXIT_FAILURE);
+	ft_putstr(message);
+	exit(EXIT_FAILURE);
 }
 
-void validate_inputs(int argc, char **argv)
+void	validate_inputs(int argc, char **argv)
 {
-    int aux;
-    int i;
-    int pid;
+	int	aux;
+	int	i;
+	int	pid;
 
-    i = 0;
-    if(argc != 3)
-        print_error("Usage: ./client <PID> <Message>\n");
-    aux = ft_strlen(argv[1]);
-    pid = ft_atoi(argv[1]);
-    while(argv[1][i])
-    {
-        if(!ft_isdigit(argv[1][i]))
-            print_error("Error: PID Invalid (PID is not a digit)\n");
-        else if(pid <= 0)
-            print_error("Error: PID Invalid (PID too small)\n/"
-            "PID 0 is reserved by the kernel\n");
-        else if (aux > 7)
-            print_error("Error: PID Invalid (PID too big)\n");
-        i++;
-    }
-    
+	i = 0;
+	if (argc != 3)
+		print_error("Usage: ./client <PID> <Message>\n");
+	aux = ft_strlen(argv[1]);
+	pid = ft_atoi(argv[1]);
+	while (argv[1][i])
+	{
+		if (!ft_isdigit(argv[1][i]))
+			print_error("Error: PID Invalid (PID is not a digit)\n");
+		else if (pid <= 0)
+			print_error("Error: PID Invalid (PID too small)\n" \
+			"PID 0 is reserved by the kernel\n");
+		else if (aux > 7)
+			print_error("Error: PID Invalid (PID too big)\n");
+		i++;
+	}
 }
 
-void message_transmit(pid_t pid, char *message)
+void	message_transmit(pid_t pid, char *message)
 {
-    int i;
-    int count_bits;
-    
-    i = 0;
-    while (message[i] != '\0')
-    {
-        count_bits = 0;
-        while (count_bits < 8)
-        {
-            if((message[i] >> count_bits) & 0b00000001)
-                kill_status = kill(pid, SIGUSR1);
-            else
-                kill_status = kill(pid, SIGUSR2);
-            if(kill_status == -1)
-                print_error("ERROR: Kill.\n");
-            count_bits++;
-            usleep(500);
-        }
-        i++;
-    }
+	int	i;
+	int	count_bits;
+    int	kill_status;
+
+	i = 0;
+	while (message[i] != '\0')
+	{
+		count_bits = 0;
+		while (count_bits < 8)
+		{
+			if ((message[i] >> count_bits) & 0b00000001)
+				kill_status = kill(pid, SIGUSR1);
+			else
+				kill_status = kill(pid, SIGUSR2);
+			if (kill_status == -1)
+				print_error("ERROR: Kill.\n");
+			count_bits++;
+			usleep(500);
+		}
+		i++;
+	}
 }
 
 int main(int argc, char **argv)
 {
-    pid_t pid;
-    char *message;
+	pid_t	pid;
+    char	*message;
 
-    validate_inputs(argc, argv);
-    pid = ft_atoi(argv[1]);
-    message = argv[2];
-    message_transmit(pid, message);
-    return (0);
+	validate_inputs(argc, argv);
+	pid = ft_atoi(argv[1]);
+	message = argv[2];
+	message_transmit(pid, message);
+	return (0);
 }
--- server.c
+++ server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbelarmi <dbelarmi@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: dbelarmi <dbelarmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 22:22:45 by dbelarmi          #+#    #+#             */
-/*   Updated: 2023/04/03 22:03:24 by dbelarmi         ###   ########.fr       */
+/*   Updated: 2023/04/04 13:18:34 by dbelarmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 
 int main(void)
 {
-   struct sigaction sig;
-   sig.sa_handler = 
-   
+   //struct sigaction sig;
+   //sig.sa_handler
 }
